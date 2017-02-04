b906421ff2354221a5945c9ba587ebf513a003ba
root <root@packer-debian-8-amd64.droplet.local>
2017-02-04 11:21:15 +0000

    test

--- src/elements/element_dtl.erl
+++ src/elements/element_dtl.erl
@@ -4,17 +4,16 @@
 -compile(export_all).
 
 render_element(Record=#dtl{}) ->
-    M = list_to_atom(nitro:to_list(Record#dtl.file) ++ "_view"),
-	Variables = M:variables(),
-    %File = case code:lib_dir(nitro:to_atom(Record#dtl.app)) of
-                %{error,bad_name} -> nitro:to_list(Record#dtl.app);
-                %A -> A end ++ "/" ++ nitro:to_list(Record#dtl.folder)
-         %++ "/" ++ nitro:to_list(Record#dtl.file) ++ "." ++ nitro:to_list(Record#dtl.ext),
-    {ok,R} = render(M, Record#dtl.js_escape, [{K,nitro:render(V)} || {K,V} <- Record#dtl.bindings] ++
-        if Record#dtl.bind_script==true -> [{script,nitro:script()}]; true-> [] end),
-    R.
+	M = list_to_atom(nitro:to_list(Record#dtl.file) ++ "_view"),
+	%File = case code:lib_dir(nitro:to_atom(Record#dtl.app)) of
+				%{error,bad_name} -> nitro:to_list(Record#dtl.app);
+				%A -> A end ++ "/" ++ nitro:to_list(Record#dtl.folder)
+			%++ "/" ++ nitro:to_list(Record#dtl.file) ++ "." ++ nitro:to_list(Record#dtl.ext),
+	{ok,R} = render(M, Record#dtl.js_escape, [{K,nitro:render(V)} || {K,V} <- Record#dtl.bindings] ++
+		if Record#dtl.bind_script==true -> [{script,nitro:script()}]; true-> [] end),
+	R.
 
 render(M, true, Args) ->
-    {ok, R} = M:render(Args),
-    {ok, nitro:js_escape(R)};
+	{ok, R} = M:render(Args),
+	{ok, nitro:js_escape(R)};
 render(M, _, Args) -> M:render(Args).
