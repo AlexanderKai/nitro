232b5e2adc836e3e70f930e1de9eeefffa0c897f
AlexanderT <AlexanderKai@users.noreply.github.com>
2017-02-06 13:47:21 +0300

    Update element_dtl.erl

--- src/elements/element_dtl.erl
+++ src/elements/element_dtl.erl
@@ -43,9 +43,8 @@ render_element(Record=#dtl{}) ->
 	erlang:display("L3"),
 	erlang:display(L3),
 	%NewRecord = Record#dtl{bindings = Record#dtl.bindings ++ }
-	erlang:display("DTL NITRO"),
-	erlang:display(L),
-	erlang:display(lists:flatten(L)),
+	erlng:display([{Bind, apply(l:l2a(CM),l:l2a(CF),[])} || {Bind, [{CM, CF}]} <- L3]),
+	
 	%File = case code:lib_dir(nitro:to_atom(Record#dtl.app)) of
 				%{error,bad_name} -> nitro:to_list(Record#dtl.app);
 				%A -> A end ++ "/" ++ nitro:to_list(Record#dtl.folder)
