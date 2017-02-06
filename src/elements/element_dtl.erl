00aafda52014c3a696711ada0c51db8ca1f18648
AlexanderT <AlexanderKai@users.noreply.github.com>
2017-02-06 17:14:13 +0300

    Update element_dtl.erl

--- src/elements/element_dtl.erl
+++ src/elements/element_dtl.erl
@@ -6,10 +6,7 @@
 render_element(Record=#dtl{}) ->
 	M = list_to_atom(nitro:to_list(Record#dtl.file) ++ "_view"),
 	Variables = M:variables(),
-	%erlang:display("Bindings"),
-	%erlang:display(Record#dtl.bindings),
-	%erlang:display("Variables"),
-	%erlang:display(Variables),
+	
 	L = lists:flatten([
 	case lists:keyfind(Var, 1, Record#dtl.bindings) of
 		false -> Var;
@@ -18,33 +15,18 @@ render_element(Record=#dtl{}) ->
 	||
 	Var <- Variables, Var /= javascript, Var /= script
 	]),
-	erlang:display("L"),
-	erlang:display(L),
+
 	L2 = [
 	{Bind, lists:sublist(l:a2l(Bind), 6, 100)}
 	||
 	Bind <- L, lists:prefix("call_",l:a2l(Bind)) == true
 	],
-	erlang:display("L2"),
-	erlang:display(L2),
+
 	L3 = [
 	{Bind, string:tokens(Call, "_")}
 	||
 	{Bind, Call} <- L2
 	],
-	erlang:display("L3"),
-	erlang:display(L3),
-	%L4 =
-	%[
-	%{Bind,{CM, lists:sublist(CF,2,100)}}
-	%||
-	%{Bind,{CM, CF}} <- L3
-	%],
-	erlang:display("-------------"),
-	erlang:display(nitro:render(apply(login,test,[]))),
-	erlang:display("-------------"),
-	%NewRecord = Record#dtl{bindings = Record#dtl.bindings ++ }
-	erlang:display([nitro:render(apply(l:l2a(CM),l:l2a(CF),[])) || {Bind, [CM, CF]} <- L3]),
 	
 	%File = case code:lib_dir(nitro:to_atom(Record#dtl.app)) of
 				%{error,bad_name} -> nitro:to_list(Record#dtl.app);
